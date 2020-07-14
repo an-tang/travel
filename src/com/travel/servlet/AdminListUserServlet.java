73a78715de73be370adba45e67d667a5ab1aa7fc
Nhan <nhantran@minhhop.net>
2020-07-14 23:57:13 +0700

    add Servlet for Admin

---
+++ src/com/travel/servlet/AdminListUserServlet.java
@@ -0,0 +1,29 @@
+package com.travel.servlet;
+
+import com.travel.helper.SessionHelpers;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet(name = "/adminListUserServlet")
+public class AdminListUserServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws
+            ServletException, IOException {
+        HttpSession currentSession = request.getSession(false);
+        boolean isAuthenticated = SessionHelpers.validateSession(currentSession);
+        if (isAuthenticated) {
+            request.getRequestDispatcher("sdAdminListUser.jsp").forward(request, response);
+        } else response.sendRedirect("/login");
+
+    }
+}
